02b55db2df1f67a0a1e7fc42ab6512a3ae174929
Illabeklemisev <ilabeklemisev218@gmail.com>
2024-03-15 18:20:08 +0200

    игра карта

---
+++ script.js
@@ -0,0 +1,55 @@
+function getRandomNumber(size) {
+    let num = Math.floor(Math.random() * size);
+    return num;
+}
+
+function getDistance(event, target) {
+    const diffX = event.offsetX - target.x;
+    const diffY = event.offsetY - target.y;
+    const dist = Math.sqrt((diffX * diffX) + (diffY* diffY));
+ return dist;
+}
+
+function getDistanceHint (distance) {
+    if (distance < 10) {
+        return "Пече!";
+    } else if (distance < 20) {
+        return "Дуже гаряче";
+    } else if (distance < 40) {
+        return "Гаряче";
+    } else if (distance < 80) {
+        return "Тепло";
+    } else if (distance < 160) {
+        return "Холодно";
+    } else if (distance < 320) {
+        return "Дуже холодно";
+    } else {
+        return "Можна замерзнути!";
+    }
+}
+
+const width = 450;
+const height = 400;
+let click = 0;
+
+const target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+}
+
+const mapElement = document.getElementById("map");
+mapElement.addEventListener("click", (event) => {
+    click++; 
+    // console.log(click);
+
+    const distance = getDistance(event, target);
+    const distanceHint = getDistanceHint(distance);
+
+    const distancElement = document.getElementById("distance");
+    distancElement.textContent = distanceHint;
+
+    if (distance < 8 ) {
+        alert("Yoa are win!")
+    }
+});
+    
